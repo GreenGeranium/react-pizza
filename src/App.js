99d3125f3596ed1cd70ee7df67df1d70c1124ee8
Maxim Grassi <grassimacsim@yandex.ru>
2023-02-25 18:34:38 +0300

    feat: получить данные с бэкенда

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import "./scss/app.scss";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Sort from "./components/Sort";
 import Categories from "./components/Categories";
 import Header from "./components/Header";
 import PizzaBlock from "./components/PizzaBlock";
-import pizzas from "./assets/pizzas.json";
 
 function App() {
+  const [pizzas, setPizzas] = useState([]);
+
+  useEffect(() => {
+    fetch("https://63fa24f4beec322c57ee3691.mockapi.io/items")
+      .then((res) => res.json())
+      .then((data) => {
+        setPizzas(data);
+        console.log(data);
+      });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header></Header>
